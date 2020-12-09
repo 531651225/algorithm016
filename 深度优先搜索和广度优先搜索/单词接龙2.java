81e486081f7f2a6524d937088e2ae6519beadfeb
bibo <bibo@mafengwo.com>
2020-12-09 19:20:23 +0800

    126. 单词接龙 II

--- 深度优先搜索和广度优先搜索/单词接龙2.java
+++ 深度优先搜索和广度优先搜索/单词接龙2.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 //给定两个单词（beginWord 和 endWord）和一个字典 wordList，找出所有从 beginWord 到 endWord 的最短转换序列。转换
 //需遵循如下规则：
 //
@@ -42,83 +44,106 @@
 //解释: endWord "cog" 不在字典中，所以不存在符合要求的转换序列。
 // Related Topics 广度优先搜索 数组 字符串 回溯算法
 // 👍 368 👎 0
-
 class 单词接龙2 {
-  List<List<String>> res = new ArrayList<List<String>>();
-  HashMap<String, Integer> distance = new HashMap<>();
-  HashMap<String, ArrayList<String>> nodeNeighbors = new HashMap<>();
-  public List<List<String>> findLadders(String start, String end, List<String> wordList) {
+  List<List<String>> res = new ArrayList<>();
+  public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
 
-    HashSet<String> dict = new HashSet<>(wordList);
-    dict.remove(start);
+    // 先将 wordList 放到哈希表里，便于判断某个单词是否在 wordList 里
+    Set<String> wordSet = new HashSet<>(wordList);
+    if (wordSet.size() == 0 || !wordSet.contains(endWord)) {
+      return res;
+    }
 
-    ArrayList<String> solution = new ArrayList<String>();
+    // 第 1 步：使用广度优先遍历得到后继结点列表 successors
+    // key：字符串，value：广度优先遍历过程中 key 的后继结点列表
+    Map<String, Set<String>> successors = new HashMap<>();
+    boolean found = bfs(beginWord, endWord, wordSet, successors);
+    if (!found) {
+      return res;
+    }
 
-    dict.add(start);
-    bfs(start, end, dict);
-    dfs(start, end, dict, solution);
+    // 第 2 步：基于后继结点列表 successors ，使用回溯算法得到所有最短路径列表
+    ArrayList<String> path = new ArrayList<>();
+    path.add(beginWord);
+    dfs(beginWord, endWord, successors, path);
     return res;
-
-
   }
 
-  private void bfs(String start, String end, Set<String> dict) {
-    for (String str : dict)
-      nodeNeighbors.put(str, new ArrayList<String>());
+  private boolean bfs(String beginWord, String endWord, Set<String> wordSet,
+      Map<String, Set<String>> successors) {
+    Queue<String> queue = new LinkedList<>();
+    queue.offer(beginWord);
 
-    Queue<String> queue = new LinkedList<String>();
-    queue.offer(start);
-    distance.put(start, 0);
+    // 标准写法，记录方法问过的单词
+    Set<String> visited = new HashSet<>();
+    visited.add(beginWord);
 
+    boolean found = false;
+    int wordLen = beginWord.length();
     while (!queue.isEmpty()) {
-      int count = queue.size();
-      boolean foundEnd = false;
-      for (int i = 0; i < count; i++) {
-        String cur = queue.poll();
-        if (cur.equals(end)) {
-          foundEnd = true;
-        }
-        int curDistance = distance.get(cur);
+      // 当前层访问过的结点，当前层全部遍历完成以后，再添加到总的 visited 集合里
+      Set<String> nextLevelVisited = new HashSet<>();
 
-        char chs[] = cur.toCharArray();
+      int currentSize = queue.size();
+      for (int i = 0; i < currentSize; i++) {
+        String currentWord = queue.poll();
+        char[] charArray = currentWord.toCharArray();
+        for (int j = 0; j < wordLen; j++) {
+          char originChar = charArray[j];
+          for (char k = 'a'; k <= 'z'; k++) {
+            if (charArray[j] == k) continue;
+            charArray[j] = k;
+            String nextWord = new String(charArray);
+            if (wordSet.contains(nextWord)) {
+              if (!visited.contains(nextWord)) {
+                if (nextWord.equals(endWord)) found = true;
 
-        for (char ch ='a'; ch <= 'z'; ch++) {
-          for (int j = 0; j < chs.length; j++) {
-            if (chs[j] == ch) continue;
-            char old_ch = chs[j];
-            chs[j] = ch;
-            String next = String.valueOf(chs);
-            if (dict.contains(next)) {
-              nodeNeighbors.get(cur).add(next);
-              if (!distance.containsKey(next)) {// Check if visited
-                distance.put(next, curDistance + 1);
-                queue.offer(next);
+                // 避免下层元素重复加入队列，这里感谢 https://leetcode-cn.com/u/zhao-da-ming/ 优化了这个逻辑
+                if (!nextLevelVisited.contains(nextWord)) {
+                  queue.offer(nextWord);
+                  nextLevelVisited.add(nextWord);
+                }
+
+                // 维护 successors 的定义
+                successors.computeIfAbsent(currentWord, a -> new HashSet<>());
+                successors.get(currentWord).add(nextWord);
               }
-
             }
-            chs[j] = old_ch;
           }
-
+          charArray[j] = originChar;
         }
-        if (foundEnd)
-          break;
       }
+
+      if (found) {
+        break;
+      }
+      visited.addAll(nextLevelVisited);
+    }
+    return found;
+  }
+
+  private void dfs(String beginWord, String endWord,
+      Map<String, Set<String>> successors,
+      ArrayList<String> path) {
+    if (beginWord.equals(endWord)) {
+      res.add(new ArrayList<>(path));
+      return;
+    }
+
+    if (!successors.containsKey(beginWord)) {
+      return;
+    }
+
+    Set<String> successorWords = successors.get(beginWord);
+    for (String nextWord : successorWords) {
+      path.add(nextWord);
+      dfs(nextWord, endWord, successors, path);
+      path.remove(path.size() - 1);
     }
   }
 
-  private void dfs(String cur, String end, Set<String> dict, ArrayList<String> solution) {
-    solution.add(cur);
-    if (end.equals(cur)) {
-      res.add(new ArrayList<String>(solution));
-    } else {
-      for (String next : nodeNeighbors.get(cur)) {
-        if (distance.get(next) == distance.get(cur) + 1) {
-          dfs(next, end, dict, solution);
-        }
-      }
-    }
-    solution.remove(solution.size() - 1);
-  }
 
 
 }
+
+
