25e955def0e7cbc6edaef3c45289a30553851df1
bibo <bibo@mafengwo.com>
2021-01-02 15:24:20 +0800

    205. 同构字符串

---
+++ 字符串/同构字符串.java
@@ -0,0 +1,62 @@
+import java.util.HashMap;
+import java.util.Map;
+
+//给定两个字符串 s 和 t，判断它们是否是同构的。
+//
+// 如果 s 中的字符可以按某种映射关系替换得到 t ，那么这两个字符串是同构的。
+//
+// 每个出现的字符都应当映射到另一个字符，同时不改变字符的顺序。不同字符不能映射到同一个字符上，相同字符只能映射到同一个字符上，字符可以映射到自己本身。
+//
+//
+//
+// 示例 1:
+//
+//
+//输入：s = "egg", t = "add"
+//输出：true
+//
+//
+// 示例 2：
+//
+//
+//输入：s = "foo", t = "bar"
+//输出：false
+//
+// 示例 3：
+//
+//
+//输入：s = "paper", t = "title"
+//输出：true
+//
+//
+//
+// 提示：
+//
+//
+// 可以假设 s 和 t 长度相同。
+//
+// Related Topics 哈希表
+// 👍 319 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class 同构字符串 {
+  public boolean isIsomorphic(String s, String t) {
+    return helper(s, t) && helper(t, s);
+  }
+
+
+  public boolean helper(String s, String t) {
+    Map<Character, Character> dic = new HashMap<>();
+    for(int i = 0; i < s.length(); i++) {
+      char schar = s.charAt(i);
+      if (dic.containsKey(schar)) {
+        if(dic.get(schar) != t.charAt(i)) return false;
+      } else {
+        dic.put(schar, t.charAt(i));
+      }
+    }
+    return true;
+  }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
