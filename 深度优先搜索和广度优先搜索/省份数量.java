4ed4af71723bf70e28a95c23406dba1bae05b338
bibo <bibo@mafengwo.com>
2021-01-19 14:21:23 +0800

    547. 省份数量

--- 深度优先搜索和广度优先搜索/省份数量.java
+++ 深度优先搜索和广度优先搜索/省份数量.java
@@ -80,6 +80,54 @@ class 省份数量 {
     }
   }
 
+
+//----------------------并查集-------------------
+
+  class UnionFind {
+    private int count = 0;
+    private int[] parent;
+
+    public UnionFind(int n) {
+      count = n;
+      parent = new int[n];
+      for (int i = 0; i < n; i++) {
+        parent[i] = i;
+      }
+    }
+
+    public int find(int p) {
+      while (p != parent[p]) {
+        parent[p] = parent[parent[p]];
+        p = parent[p];
+      }
+      return p;
+    }
+
+    public void union(int p, int q) {
+      int rootP = find(p);
+      int rootQ = find(q);
+      if (rootP == rootQ) return;
+      parent[rootP] = rootQ;
+      count--;
+    }
+
+  }
+
+  public int findCircleNum(int[][] isConnected) {
+    int m = isConnected.length;
+    if(m == 0) return 0;
+    int n = isConnected[0].length;
+    UnionFind u = new UnionFind(m);
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if(isConnected[i][j] == 1) {
+          u.union(i, j);
+        }
+      }
+    }
+    return u.count;
+  }
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
