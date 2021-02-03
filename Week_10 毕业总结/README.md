00c37efe0f7058242aedcd8d61a26b1656e2bc7b
bibo <bibo@mafengwo.com>
2021-02-03 10:21:42 +0800

    61. 旋转链表

--- Week_10 毕业总结/README.md
+++ Week_10 毕业总结/README.md
@@ -17,6 +17,7 @@
 - [169. 多数元素](https://github.com/531651225/algorithm016/blob/master/%E6%95%B0%E7%BB%84/%E5%A4%9A%E6%95%B0%E5%85%83%E7%B4%A0.java)
 - [36. 有效的数独](https://github.com/531651225/algorithm016/blob/master/%E6%95%B0%E7%BB%84/%E6%9C%89%E6%95%88%E7%9A%84%E6%95%B0%E7%8B%AC.java)
 - [41. 缺失的第一个正数](https://github.com/531651225/algorithm016/blob/master/%E6%95%B0%E7%BB%84/%E7%BC%BA%E5%A4%B1%E7%9A%84%E7%AC%AC%E4%B8%80%E4%B8%AA%E6%AD%A3%E6%95%B0.java)
+- [442. 数组中重复的数据](https://github.com/531651225/algorithm016/blob/master/%E6%95%B0%E7%BB%84/%E7%BC%BA%E5%A4%B1%E7%9A%84%E7%AC%AC%E4%B8%80%E4%B8%AA%E6%AD%A3%E6%95%B0.java)
 
 
 #### 二叉树 - [知识点](http://note.youdao.com/s/Stvimhoh)
--- 链表/旋转链表.java
+++ 链表/旋转链表.java
@@ -34,29 +34,40 @@
  * }
  */
 class 旋转链表 {
+
+
+  //这题k是非负数，但k有可能比链表的长度还要大，所以先要计算链表的长度len，需要旋转的步数就是（k%len）。一种比较简单的方式就是先把链表连接成一个环，然后再把链表在某个合适的位置断开。
+  //
+  //我们可以使用两个指针，一个快指针fast从头开始遍历直到走到链表的末尾，然后再把链表串成一个环形。还一个指针slow也是从头开始，走（len-k%len）步就是我们要返回的链表头，这里可能有点疑问，为什么不是走（k%len）步，这是因为我们需要把链表后面的（k%len）个移到前面，因为单向链表我们没法从后往前遍历，所以我们只能从前往后移动（len-k%len）步。但实际上操作的时候会少走一步，具体来举个例子看一下，这里就以示例1为例画个图来看一下
+  //
+  //作者：sdwwld
+  //链接：https://leetcode-cn.com/problems/rotate-list/solution/61-xuan-zhuan-lian-biao-tu-wen-xiang-jie-by-sdwwld/
+  //来源：力扣（LeetCode）
+  //著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
   public ListNode rotateRight(ListNode head, int k) {
-    if (head == null)
+    if(head == null || head.next == null) {
       return head;
+    }
     ListNode fast = head, slow = head;
     int len = 1;
-    //统计链表的长度，顺便找到链表的尾结点
-    while (fast.next != null) {
-      len++;
+    while(fast.next != null) {
       fast = fast.next;
+      len++;
     }
-    System.out.println(len);
-
-
+    //首尾相连，先构成环
     fast.next = head;
-    int setp = len - k % len;
-
-    for (int i = setp; i > 1; i--) {
+    //慢指针移动的步数
+    int step = len - k % len;
+    //移动步数，这里大于1实际上是少移了一步
+    for (int i = 0; i < step - 1 ; i++) {
       slow = slow.next;
     }
+    //temp就是需要返回的结点
     ListNode temp = slow.next;
+    //因为链表是环形的，slow就相当于尾结点了，
+    //直接让他的next等于空
     slow.next = null;
     return temp;
-
   }
 }
 //leetcode submit region end(Prohibit modification and deletion)
