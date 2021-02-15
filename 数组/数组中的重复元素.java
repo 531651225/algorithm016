2d957fd3787c36ecd7c0ea62a8d272063ea34074
bibo <bibo@mafengwo.com>
2021-02-15 09:42:34 +0800

    442. 数组中重复的数据

--- 数组/数组中的重复元素.java
+++ 数组/数组中的重复元素.java
@@ -41,5 +41,17 @@ class 数组中的重复元素 {
     nums[j] = temp;
   }
 
+
+  public List<Integer> findDuplicates(int[] nums) {
+    List<Integer> res = new ArrayList<>();
+    for (int i = 0; i < nums.length; ++i) {
+      int index = Math.abs(nums[i])-1;
+      if (nums[index] < 0)
+        res.add(Math.abs(index+1));
+      nums[index] = -nums[index];
+    }
+    return res;
+  }
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
